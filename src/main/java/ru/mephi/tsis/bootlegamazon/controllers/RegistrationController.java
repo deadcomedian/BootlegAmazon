61b96c4a0bd990a700f302ce8708136a95f6dcb8
deadcomedian <ilya-sharov-2000@mail.ru>
2022-12-17 00:51:48 +0300

    + registration mechanism

--- src/main/java/ru/mephi/tsis/bootlegamazon/controllers/RegistrationController.java
+++ src/main/java/ru/mephi/tsis/bootlegamazon/controllers/RegistrationController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.mephi.tsis.bootlegamazon.dao.entities.UserEntity;
 import ru.mephi.tsis.bootlegamazon.services.implementations.UserService;
@@ -14,34 +15,43 @@ import ru.mephi.tsis.bootlegamazon.services.implementations.UserService;
 import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/registration")
 public class RegistrationController {
-    @Autowired
-    private UserService userService;
 
-    @GetMapping("/registration")
+    private UserService userService;
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
     public String registration(Model model) {
         model.addAttribute("userForm", new UserEntity());
         return "registration";
     }
 
-    @PostMapping("/registration/create")
-    public String addUser(@ModelAttribute("userForm") @Valid UserEntity userForm, BindingResult bindingResult, Model model) {
+    @PostMapping("/create")
+    public String addUser(
+            @ModelAttribute("userForm") @Valid UserEntity userForm,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ) {
 
         if (userForm.getName().equals("") || userForm.getPhone().equals("") || userForm.getLogin().equals("") || userForm.getPassword().equals("") || userForm.getPasswordConfirm().equals("")) {
-            model.addAttribute("fieldError", "Все поля должны быть заполнены!");
-            return "redirect:/registration/";
+            redirectAttributes.addFlashAttribute("fieldError", "Все поля должны быть заполнены!");
+            return "redirect:/registration";
         }
 
         if (bindingResult.hasErrors()) {
-            return "redirect:/registration/";
+            return "redirect:/registration";
         }
         if (!userForm.getPassword().equals(userForm.getPasswordConfirm())){
-            model.addAttribute("passwordError", "Пароли не совпадают");
-            return "redirect:/registration/";
+            redirectAttributes.addFlashAttribute("passwordError", "Пароли не совпадают");
+            return "redirect:/registration";
         }
         if (!userService.createUser(userForm.getName(), userForm.getPhone(), userForm.getLogin(), userForm.getPassword())){
-            model.addAttribute("usernameError", "Пользователь с таким именем уже существует");
-            return "redirect:/registration/";
+            redirectAttributes.addFlashAttribute("usernameError", "Пользователь с таким именем уже существует");
+            return "redirect:/registration";
         }
         return "redirect:/items/all?page=0";
     }
