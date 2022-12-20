0b51ab254f1a6ef2a327a12b77e548fc0ef80a19
Tatyana2000 <rytata2000@mail.ru>
2022-12-21 00:34:33 +0300

    Редактирование профиля

--- src/main/java/ru/mephi/tsis/bootlegamazon/configs/WebSecurityConfig.java
+++ src/main/java/ru/mephi/tsis/bootlegamazon/configs/WebSecurityConfig.java
@@ -7,50 +7,55 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+/*
     @Override
     protected void configure(HttpSecurity security) throws Exception
     {
         security.httpBasic().disable();
     }
 
+*/
 
+    @Bean
+    public PasswordEncoder encoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
-//    @Bean
-//    public PasswordEncoder encoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.authorizeRequests().antMatchers("/style/**").permitAll();
-//
-//        httpSecurity.authorizeRequests()
-//                //Доступ только для не зарегистрированных пользователей
-//                .antMatchers("/items/**").permitAll()
-//                //.antMatchers("/items/**").not().fullyAuthenticated()
-//                //Доступ только для пользователей с ролью Администратор и Менеджер
-//                //.antMatchers("/category/**").hasAuthority("Администратор")
-//                //.antMatchers("/category/**").hasAuthority("Менеджер")
-//                //Доступ разрешен всем пользователей
-//                //Все остальные страницы требуют аутентификации
-//                .anyRequest().authenticated()
-//                .and()
-//                //Настройка для входа в систему
-//                .formLogin()
-//                .loginPage("/login")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-//
-//    }
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/style/**").permitAll();
 
+        httpSecurity.authorizeRequests()
+                //Доступ только для не зарегистрированных пользователей
+                //.antMatchers("/registration").not().fullyAuthenticated()
+                .antMatchers("/registration").not().fullyAuthenticated()
+                .antMatchers("/login").not().fullyAuthenticated()
+                .antMatchers("/items/**").permitAll()
+                //.antMatchers("/registration").permitAll()
+                //.antMatchers("/items/**").not().fullyAuthenticated()
+                //Доступ только для пользователей с ролью Администратор и Менеджер
+               //.antMatchers("/category/**").hasAuthority("Администратор")
+                //.antMatchers("/category/**").hasAuthority("Менеджер")
+                //Доступ разрешен всем пользователей
+                //Все остальные страницы требуют аутентификации
+                .anyRequest().authenticated()
+                .and()
+                //Настройка для входа в систему
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/items/all?page=0")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/items/all?page=0");
 
+    }
 
 }
 
--- src/main/java/ru/mephi/tsis/bootlegamazon/controllers/CustomerProfileController.java
+++ src/main/java/ru/mephi/tsis/bootlegamazon/controllers/CustomerProfileController.java
@@ -5,16 +5,15 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.mephi.tsis.bootlegamazon.dao.entities.UserAuth;
 import ru.mephi.tsis.bootlegamazon.dao.entities.UserEntity;
 import ru.mephi.tsis.bootlegamazon.dao.repositories.UserAuthRepository;
 import ru.mephi.tsis.bootlegamazon.dao.repositories.UserRepository;
+import ru.mephi.tsis.bootlegamazon.models.CustomerProfile;
 import ru.mephi.tsis.bootlegamazon.services.implementations.UserService;
-import org.springframework.security.core.userdetails.User;
+
 
 @Controller
 @RequestMapping("/profile")
@@ -42,15 +41,35 @@ public class CustomerProfileController {
 
     @GetMapping("/{id}")
     public String anyProfile (@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal UserDetails currentUser) {
-        //UserEntity user = userRepository.findById(id).get();
-        //String username = String.valueOf();
         UserAuth loggedUser = userAuthRepository.findByUsername(currentUser.getUsername());
-        UserAuth user = userAuthRepository.findById(id).get();
+        UserEntity user = userRepository.findById(id).get();
         if (!loggedUser.getId().equals(user.getId())) {
             return "error-page";
         }
         model.addAttribute("currentUser", user);
         return "profile";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal UserDetails currentUser) {
+        UserAuth loggedUser = userAuthRepository.findByUsername(currentUser.getUsername());
+        UserEntity user = userRepository.findById(id).get();
+        if (!loggedUser.getId().equals(user.getId())) {
+            return "error-page";
+        }
+        model.addAttribute("currentUser", user);
+        return "profile-edit-page";
+    }
+
+    @PostMapping("/saveedit")
+    public String saveEdit(@ModelAttribute("user") UserEntity user, Model model, @AuthenticationPrincipal UserDetails currentUser, RedirectAttributes redirectAttributes) {
+        UserAuth loggedUser = userAuthRepository.findByUsername(currentUser.getUsername());
+        if (!user.getPassword().equals(user.getPasswordConfirm())){
+            redirectAttributes.addFlashAttribute("passwordError", "Пароли не совпадают");
+            return "redirect:/registration";
+        }
+        userService.update(loggedUser.getId(), user.getName(), user.getPassword());
+        return "redirect:/profile/"+user.getId();
     }
 
 }
